a54735a913501b24f460029aa9539b7cdcb72c60
lucky-leaf <yx1030@live.com>
2018-04-26 05:44:25 +0800

    Add component HomeIcons

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -2,18 +2,21 @@
   <div>
     <home-header></home-header>
     <home-swiper></home-swiper>
+    <home-icons></home-icons>
 </div>
 </template>
 
 <script>
 import HomeHeader from './HomeHeader'
 import HomeSwiper from './HomeSwiper'
+import HomeIcons from './HomeIcons'
 
 export default {
   name: 'Home',
   components: {
     HomeHeader,
-    HomeSwiper
+    HomeSwiper,
+    HomeIcons
   }
 }
 </script>
---
+++ src/views/Home/HomeIcons.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="icons">
+    <div class="icon">
+      <div class="icon-img">
+        <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png">
+      </div>
+      <p class="icon-desc">热门景点</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeIcons'
+}
+</script>
+
+<style lang="less" scoped>
+  @import '../../assets/css/variables.less';
+
+  .icons {
+    overflow: hidden;
+    height: 0;
+    padding-bottom: 50%;
+
+    .icon {
+      position: relative;
+      overflow: hidden;
+      width: 25%;
+      float: left;
+      height: 0;
+      padding-bottom: 25%;
+
+      .icon-img {
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0.44rem;
+        box-sizing: border-box;
+        padding: 0.1rem;
+        .icon-img-content {
+          display: block;
+          margin: 0 auto;
+          height: 100%;
+        }
+      }
+
+      .icon-desc {
+        position: absolute;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        height: 0.44rem;
+        line-height: 0.44rem;
+        text-align: center;
+        color: @font-color-dark;
+        font-size: 0.24rem;
+      }
+    }
+  }
+</style>
